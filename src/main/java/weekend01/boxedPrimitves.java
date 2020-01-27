7b8ec745f3049709f4316d972f94586b1a53bb29
bary_dd <darek_dan@hotmail.com>
2020-01-27 23:26:08 +0100

    added application "boxedPrimitives.java" - exercise 2

---
+++ src/main/java/weekend01/boxedPrimitves.java
@@ -0,0 +1,12 @@
+package weekend01;
+
+public class boxedPrimitves {
+    public static void main(String[] args) {
+        Integer intValBoxed = 10;
+        Long longValBoxed = 10L;
+        Short shortValBoxed = 10;
+        Double doubleValBoxed = 10.0;
+        Float floatValBoxed = 10.0F;
+    }
+}
+// this app should end with error but i got no error :/
