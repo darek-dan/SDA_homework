ef2b8c1514fea8bc639b7c04138fff98ebc5dbfd
bary_dd <darek_dan@hotmail.com>
2020-02-02 09:29:10 +0100

    added application "Tree.java" - exercise 2

---
+++ src/main/java/weekend02/Tree.java
@@ -0,0 +1,65 @@
+package weekend02;
+
+import java.util.Scanner;
+
+public class Tree {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int base;
+        do {
+            System.out.println("Podaj wysokość choinki w zakresie od 3 do 101");
+            base = scanner.nextInt();
+            if (base < 3 || base > 101) {
+                System.out.println("Podałeś liczbę która nie mieści się w zakresie");
+            }
+        } while (base < 3 || base > 101);
+
+        int numberOfStars = 1;
+        int numberOfSpaces;
+        int numberOfLines;
+
+        numberOfLines = base;
+
+
+        while (numberOfLines > 0) {
+            numberOfSpaces = numberOfLines -1;
+            while (numberOfSpaces > 0) {
+                System.out.print(" ");
+                numberOfSpaces --;
+            }
+
+            int i;
+            i = numberOfStars;
+            while (i > 0) {
+                System.out.print("*");
+                --i;
+            }
+
+            numberOfStars += 2;
+            -- numberOfLines;
+            System.out.println();
+        }
+
+        numberOfSpaces = base - 2;
+        while (numberOfSpaces > 0) {
+            System.out.print(" ");
+            -- numberOfSpaces;
+        }
+
+        System.out.println("| |");
+    }
+
+
+}
+
+
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+    | |
+ */
